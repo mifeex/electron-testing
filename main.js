76169c5ad2dca0b984fa78a79a6bf6dc76004419
Evgeniy <60233967+mifeex@users.noreply.github.com>
2020-05-26 01:49:27 -0700

    google search + electron

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: '100%',
+    height: '100%',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    autoHideMenuBar: true,
   })
 
   // and load the index.html of the app.
